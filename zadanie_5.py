f8f2b558f8266602a5579a02693eb5b4bff700be
sebsow9 <96175415+sebsow9@users.noreply.github.com>
2023-10-13 15:20:11 +0200

    zadanie 5 skonczone

---
+++ zadanie_5.py
@@ -0,0 +1,13 @@
+#a
+
+#przy uzyciu string
+x = input("podaj liczbe: ")
+print("Wprowadzona przez Ciebie liczba to: %s " %x)
+
+#przy uzyciu int
+
+x = int(input("podaj liczbe: "))
+print("Wprowadzona przez ciebie liczba to: %d" %x)
+
+#b
+print("twoja liczba to:",input("Podaj liczbe: ") )
