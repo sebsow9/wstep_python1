c5f33a22d1f0fc6ec30f5509e6bb8cc32832da90
sebsow9 <96175415+sebsow9@users.noreply.github.com>
2023-10-13 15:01:45 +0200

    zadanie 4

---
+++ zadanie_4.py
@@ -0,0 +1,37 @@
+#a
+print("Sebastian")
+print("Sowa")
+print("20")
+print("pizza")
+print("kot")
+print(5/7)
+
+#b
+print("Sebastian, Sowa, 20, pizza, kot", 5/7)
+
+#c
+print(round(5/7, 1), round(5/7, 3), round(5/7, 5), round(5/7, 10))
+
+#d(ze zmiennymi)
+
+#a.2
+imie = "Sebastian"
+nazwisko = "Sowa"
+wiek = 20
+potrawa = "pizza"
+zwierze = "kot"
+wynik = float(5/7)
+
+
+print(imie)
+print(nazwisko)
+print(wiek)
+print(potrawa)
+print(zwierze)
+print(wynik)
+
+#b.2
+print(imie, nazwisko, wiek, potrawa, zwierze ,wynik)
+
+#c.2
+print(round(wynik, 1), round(wynik, 3), round(wynik, 5), round(wynik, 10))
