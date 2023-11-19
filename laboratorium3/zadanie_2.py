418def174cd53ae9027072a2a7f58d1d1adcae63
sebsow9 <96175415+sebsow9@users.noreply.github.com>
2023-11-19 19:49:35 +0100

    naprawione i gotowe

--- laboratorium3/zadanie_2.py
+++ laboratorium3/zadanie_2.py
@@ -1,11 +1,11 @@
 import numpy
-print("Podaj wymiar pierwszej macierzy m x n")
-
-rows_m_1 = int(input("m: "))
-columns_n_1 = int(input("n: "))
+licznik = 1
+counter = 1
 
 def creating_matrix(rows, columns):
-    
+    global matrix_2_before
+    global matrix_1_before
+    global counter
     matrix = numpy.empty((rows,columns)) #funkcja numpy tworzaca macierz m x n za pomoca zagniezdzenia tablic
     
     for z in range(0,columns):
@@ -16,14 +16,20 @@ def creating_matrix(rows, columns):
             
             matrix[g][z] = int(input())
 
-
+    matrix = cutting_matrix(matrix, rows, columns)
+    if (counter == 1):
+        matrix_1_before = matrix
+    elif (counter == 2):
+        matrix_2_before = matrix
+    matrix = det_times_matrix(matrix, rows, columns)
+    counter = counter + 1
     return matrix
 
 def det_matrix(matrix):
     return numpy.linalg.det(matrix)
     
 def det_times_matrix(matrix, rows, columns):
-    final_matrix = numpy.empty(rows, columns)
+    final_matrix = numpy.empty((rows, columns))
     det = det_matrix(matrix)
     for z in range(0,rows):
         for g in range(0, columns):
@@ -35,13 +41,12 @@ def det_times_matrix(matrix, rows, columns):
 
 def matrix_multiplication(rows_1, rows_2, columns_2, matrix1, matrix2): #opcjonalnie zamiast rows_2 moze byc columns_1
     final_matrix = numpy.empty((rows_1,columns_2))
-    
-    
     for z in range(0,rows_1):
         for g in range(0,columns_2):
-                for c in range(0,rows_2): #moze byc columns 1
-                    value = value + (matrix1[z][c]*matrix2[c][g])
-                final_matrix[z][g] = value
+            value = 0
+            for c in range(0,rows_2): #moze byc columns 1
+                value = value + (matrix1[z][c]*matrix2[c][g])
+            final_matrix[z][g] = value
 
 
     
@@ -49,15 +54,78 @@ def matrix_multiplication(rows_1, rows_2, columns_2, matrix1, matrix2): #opcjona
     
     return final_matrix
 
+def cutting_matrix(matrix, rows, columns):
+    global licznik
+    global columns_n_1
+    global columns_n_2
+    global rows_m_1
+    global rows_m_2
 
+    if (rows>columns):
+        
+        final_matrix = numpy.empty((columns, columns))
+        
+        
+        
+        if (licznik == 1):
+            rows_m_1 = columns_n_1
+        elif (licznik == 2):
+            rows_m_2 = columns_n_2
+        
+        for z in range(0,columns):
+            for g in range(0,columns):
+                final_matrix[z][g] = matrix[z][g]
+        
+    elif (columns>rows):
+        
+        final_matrix = numpy.empty((rows, rows))
+        
+        
+        
+        if (licznik == 1):
+            columns_n_1 = rows_m_1
+        elif (licznik == 2):
+            columns_n_2 = rows_m_2
 
+        for z in range(0,rows):
+            for g in range(0,rows):
+                final_matrix[z][g] = matrix[z][g]
+    else:
+        final_matrix = matrix #bez tego nie dziala???
+    licznik = licznik + 1
+                
+    return final_matrix
+
+print("Podaj wymiar pierwszej macierzy m x n")
+
+rows_m_1 = int(input("m: "))
+columns_n_1 = int(input("n: "))
 matrix_1 = creating_matrix(rows_m_1, columns_n_1)
+
 print("Podaj wymiar drugiej macierzy m x n")
 
 rows_m_2 = int(input("m: "))
 columns_n_2 = int(input("n: "))
+
+matrix_1_before = numpy.empty((rows_m_1,columns_n_1))
+
+matrix_2_before = numpy.empty((rows_m_2,columns_n_1))
 matrix_2 = creating_matrix(rows_m_2, columns_n_2)
 
-#if (rows_m_1 == columns_n_2 and rows_m_2 == columns_n_1):
 
+
+
+
+if (rows_m_2 == columns_n_1):
+    print("Mnozymy dwie macierze ze soba kazda z nich pomnozona przez sume ich wyznacznikow")
+    print("")
+    print("Pierwsza macierz i jej Det:", det_matrix(matrix_1_before) )
+    print(matrix_1)
+    print("") 
+    print("Druga macierz", det_matrix(matrix_2_before))
+    print(matrix_2)
+    print("")
+    print("Wynik:")
+    print(matrix_multiplication(rows_m_1, rows_m_2, columns_n_2, matrix_1, matrix_2))
+    
     
