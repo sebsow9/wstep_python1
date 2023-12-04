85fbdce6ef1e304d947f01be809f09259a045831
sebsow9 <96175415+sebsow9@users.noreply.github.com>
2023-12-04 17:57:58 +0100

    dziala

--- laboratorium4/zadanie6.py
+++ laboratorium4/zadanie6.py
@@ -1,5 +1,5 @@
 import random
-import matplotlib.pyplot
+import numpy as np
 
 print("Podaj rozmiary tablicy N x M x O: ")
 
@@ -16,6 +16,31 @@ matrix = str(creating_matrix(N, M, O))
 with open('example.txt', 'w' ) as file: 
     file.write(matrix)
 
+import matplotlib.pyplot as plt
+
+
 with open('example.txt', 'r') as file:
-    content = file.read()
+    content = eval(file.read())
+
+
+flat_data = [item for sublist1 in content for sublist2 in sublist1 for item in sublist2 ]
+
+bin_edges = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+
+plt.hist(flat_data, bins=bin_edges, color='g', edgecolor='black', linewidth=1.3)
+plt.title('Histogram of Data')
+plt.xticks(range(0, 10001, 1000))
+plt.xlabel('Values')
+plt.ylabel('Frequency')
+plt.show()
+
+hist, _ = np.histogram(flat_data, bins=bin_edges)
+
+
+for i, count in enumerate(hist):
+    bin_range = f"[{bin_edges[i]}-{bin_edges[i+1]}]"
+    bars = "=" * count
+    print(f"{bin_range.ljust(12)} {bars}")
+
+
 
