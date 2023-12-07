19c9d157ef7ecc7c44a3b5ee717c235ed6ee57a4
sebsow9 <96175415+sebsow9@users.noreply.github.com>
2023-12-07 20:59:46 +0100

    wiersze i kolumny sprawdzone

--- laboratorium5/zadanie2.py
+++ laboratorium5/zadanie2.py
@@ -1,10 +1,11 @@
 import random
 import matplotlib.pyplot as plt
-
+import numpy as np
 N_queens = int(input("Podaj ilość hetmanow: "))
 
 tableofpositions = [(random.randint(1,8), random.randint(1,8)) for _ in range(N_queens)]
-
+rowtable = []
+columntable =[]
 #zmienna = int(str(tableofpositions[0])[3:4])
 
 print(tableofpositions)
@@ -43,10 +44,50 @@ def draw_chessboard():
     ax.set_xlim(0, 8)
     ax.set_ylim(0, 8)
     
+    
+        
+    
+
 
     #dla upiekszenia wyrownujemy rysunek do kwadratu
     ax.set_aspect('equal', adjustable='box')
 
     plt.show()
+
+for z in tableofpositions:
+    
+    rowtable = rowtable + [z[0]]
+    
+    columntable = columntable +[z[1]]
+
+def row_column(table, table2):
+    
+    for g in range(len(table)): 
+        
+        for f in range(len(table)): #tablica bez elemntu g, wiec sprawdzamy kazdy element po kolei oprocz g
+            
+            if table[f] == table[g] and f!=g: #rows check
+                return True
+    
+    for a in range(len(table2)):
+        
+        for b in range(len(table2)): #tablica bez elemntu a, wiec sprawdzamy kazdy element po kolei oprocz a
+            
+            if table2[b] == table2[a] and b!=a: #columns check
+                return True       
+        
+
+
+if row_column(rowtable, columntable) == True:
+    
+    print("Szachują się")
+
+else:
+    
+    print("Nie szachują się")
+
+
+
+
 
 draw_chessboard()
